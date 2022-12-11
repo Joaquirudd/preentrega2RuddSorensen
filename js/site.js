ceceff3e787595e0bb2d4b673bd2b4eaaaaea09b
Joaquina Rudd Sorensen <joaquinarudds00@gmail.com>
2022-12-11 19:47:48 +0100

    Segunda preEntrega

--- js/site.js
+++ js/site.js
@@ -1,36 +1,48 @@
 /* 
-
-Realicé un algoritmo que calcula las calficaciones de los alumnos.
-Primero debe ingresar el nombre del alumno a través del prompt. Luego, de la misma forma, hay que ingresar tres números, que serían las calificaciones.
-El resultado final del alumno se mostrará en la consola justo a su nombre.
-Este ciclo se repetirá hasta que el usuario escriba "esc" en el prompt donde pregunta por el nombre y el número 00 en los que pregunta por las notas de ellos.
-Al final de todo, saldrá un mensaje deseándoles felices vacaciones.
-
+realicé un algoritmo que ordena a los alumnos dependiendo si aprovaron o no.
+primero debe ingresar el nombre del alumno. Luego, de esta misma forma, un número que será la calificación del alumno.
+a medida que vaya escribiando alumnos, estos se imprimirán en la consola. Al mismo tiempo se agruparán en dos arrays: el de los aprovados y el de los desaprovados. Tambien los nombres aparecerán en un objeto de alunos en general.
+al final de todo, saldrán unos carteles dependiendo de la cantidad de alumnos aprovados o desaprovados.
 */
 
+
 //Declaración de las variables:
-let nombreAlumno = prompt("Ingrese el nombre del alumno/a; Si desea salir, escriba esc"); //nombre del alumno
-let calificacion1 = Number(prompt("Ingrese la primera nota de " + nombreAlumno + " Si desea salir, escriba 00")); //primera calificacion a sumar
-let calificacion2 = Number(prompt("Ingrese la segunda nota de " + nombreAlumno + " Si desea salir, escriba 00")); //segunda calificacion a sumar
-let calificacion3 = Number(prompt("Ingrese la tercer nota de " + nombreAlumno + " Si desea salir, escriba 00")); //tercer calificacion a sumar
-let calificacionFinal = ((calificacion1 + calificacion2 + calificacion3) / 3); //se suman las tres calificaciones previamente ingresadas y se divide el resultado 
+let nombreAlumno = prompt("Ingrese el nombre del alumno"); //Nombre del alumno
+let calificacionFinal = Number(prompt("Ingrese la calificacion final de alumno")); //Calificacion final del alumno
 
+//Arrays donde se almacenarán los alumnos aprovados y desaprovados
+const alumnosAprovados = []
+const alumnosDesaprovados = []
 
-function calificacionAlumnos(mensaje) { 
-    while ((nombreAlumno != "esc" || "ESC") && (calificacion1 != 00) && (calificacion2 != 00) && (calificacion3 != 00)) { //El while permitirá que, cuando el usuario quiera, el programa finalice.
-        if (calificacionFinal >= 7){ //Si la calificacion final es mayor o igual a 7, el alumno aprobó
-            console.log(nombreAlumno + " aprobó la materia con una calificación de " + calificacionFinal);
-        } else{ //Si la calificacion es menor a 7, el alumno desaprobó
-            console.log (nombreAlumno + " desaprobó la materia con una calificación de " + calificacionFinal);
-        }
-        nombreAlumno = prompt("Ingrese el nombre del alumno/a");
-        calificacion1 =Number(prompt("Ingrese la primera nota de " + nombreAlumno));
-        calificacion2 =Number(prompt("Ingrese la segunda nota de " + nombreAlumno));
-        calificacion3 =Number(prompt("Ingrese la tercer nota de " + nombreAlumno));
-        calificacionFinal = ((calificacion1 + calificacion2 + calificacion3) / 3);
-    }
-    console.log(mensaje)
+//Objeto donde se almacenarán todos los alumnos
+const alumnosGeneral = {
+    nombre: nombreAlumno
 }
 
-calificacionAlumnos("Felices Vacaciones"); //mensaje de felices vacaciones al finalizar
+while ((nombreAlumno != "esc" || "ESC") && (calificacionFinal != 00)){
+if (calificacionFinal >= 7){ //Si la calificacion es mayor o igual a 7, el nombre del alumno pasará al array de aprovados. Al mismo tiempo su nombre aparecerá en el objeto.
+    alumnosAprovados.push(nombreAlumno);
+    alumnosGeneral.nombre= nombreAlumno;
+    console.log (alumnosAprovados)
+    console.log (alumnosGeneral)
+} else{ //Si la calificacon es menor a 7, el nombre del alumno pasará al array de desaprovados. Al mismo tiempo su nombre aparecerá en el objeto.
+    alumnosDesaprovados.push(nombreAlumno);
+    alumnosGeneral.nombre= nombreAlumno;
+    console.log (alumnosDesaprovados)
+    console.log (alumnosGeneral)
+}
 
+nombreAlumno = prompt("Ingrese el nombre del alumno/a");
+calificacionFinal = Number(prompt("Ingrese la calificacion final de alumno"));
+}
+
+
+//por cada alumno aprovado saldrá un cartel de felices vacaciones
+alumnosAprovados.forEach( ()=> {
+    console.log("felices vacaciones")
+})
+
+// por cada alumno desaprovado saldrá un cartel de nos vemos en diciembre
+alumnosDesaprovados.forEach( ()=> {
+    console.log("Nos vemos en diciembre")
+})
